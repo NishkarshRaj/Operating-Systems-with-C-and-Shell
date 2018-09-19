dbaea332d18b876cbeb6b6271b7e34a953695e8d
= <=>
2018-09-19 19:09:05 +0530

    modified basicreadwrite to pipe program

--- pipesystemcall/basicreadwritepipe.c
+++ pipesystemcall/basicreadwritepipe.c
@@ -13,12 +13,17 @@ int main()
     /* write pipe */
     printf("Enter a string of size less than 32: ");
 scanf("%s",&w);
-    write(p[1],(char*)w,sizeof(w));  
+    write(p[1],(char*)w,sizeof(w));  //In c typecast only as (datatype)variable
 printf("Written content to the buffer\n");
-
 printf("Lets read the content from the buffer\n");    
         read(p[0],(char*)r, sizeof(r)); 
-   printf("The value read from the buffer is: %s",r); 
+   printf("The value read from the buffer is: %s\n",r); 
  return 0; 
 } 
 
+
+//This is an example of simplex!!! no its an exmple of 1 user sending and receiving the data
+//for multiple users we need forking process
+//for simplex: 1 user enters the data and 1 user reads
+//for half duplex: 1 user sends the data other sees it and replies to it and repeat this process
+//for full duplex: both users sends and recives data simultanieously as done in controlled case of fork process
