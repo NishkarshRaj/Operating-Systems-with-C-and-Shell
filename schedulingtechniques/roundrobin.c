48f4b038f446af0b74a0025b17c3ccf175cd2eea
= <=>
2018-09-27 08:23:31 +0530

    Round Robin about to be completedgit add .!

--- schedulingtechniques/roundrobin.c
+++ schedulingtechniques/roundrobin.c
@@ -7,7 +7,7 @@ printf("Enter the number of processes: ");
 scanf("%d",&n);
 printf("Enter the time quantum: ");
 scanf("%d",&q);
-int pnum[n],burst[n],wait[n],turn[n],flag[n],count;
+int pnum[n],burst[n],wait[n],turn[n],flag[n],count,b[n],last[i];;
 for(i=0;i<n;i++)
 {
 printf("Enter the details for process number %d\n",i+1);
@@ -18,56 +18,63 @@ scanf("%d",&burst[i]);
 flag[i]=0;
 wait[i]=0;
 turn[i]=0;
+last[i]=0;
 }
-//////////////////////////////////////////////////////////////////
 for(i=0;i<n;i++)
 {
+b[i]=burst[i]; //storing burst time as they will be changed later
 turn[i]=turn[i]+burst[i];
 t=t+burst[i];
 }
-printf("Total compilation will take %d time",t);
+printf("Total compilation will take %d time\n",t);
+/////////////////////////////////////////
 while(flag1!=1)
 {
-for(i=0;i<n;i++)
-{
-if(burst[i]>q)
-	{
-if(wait[i]==0)
-wait[i]=t1;
-else
-{
-k=t1-wait[i];
-wait[i]=k;
-}
-	printf("Process %d is running!!\n",i+1);
-	t1=t1+q;
-	burst[i]=burst[i]-q;
-	}
-else
-	{
-if(wait[i]==0)
-wait[i]=t1;
-else
-{
-k=t1-wait[i];
-wait[i]=k;
-}
-	printf("Process %d is running!!\n",i+1);
-	t1=t1+burst[i];
-	flag[i]=1;
-	}
-}
 	for(i=0;i<n;i++)
 	{
-	if(flag[i]==1)
-	count++;
-	if(count==n)
-	flag1=1;
+	if((burst[i]>q)&&(flag[i]==0))
+		{
+		if(wait[i]==0)
+		{
+		wait[i]=t1;
+		}
+		else
+		{
+		wait[i]=wait[i]+(t1-last[i]);
+		}
+		printf("Process %d is running!!\n",i+1);
+		t1=t1+q;
+		burst[i]=burst[i]-q;
+		last[i]=t1;
+		}
+	else if ((burst[i]<=q)&&(flag[i]==0))
+		{
+	if(wait[i]==0)
+	wait[i]=t1;
+	else
+	{
+	wait[i]=wait[i]+(t1-last[i]);
 	}
+		printf("Process %d is running!!\n",i+1);
+		t1=t1+burst[i];
+		flag[i]=1;
+		last[i]=t1;
+		}
+	}
+	count=0;
+		for(i=0;i<n;i++)
+		{
+		if(flag[i]==1)
+		count=count+1;
+		}
+if(count==n)
+flag1=1;
 }
 ////////////////////////////////////////////////////////////
 for(i=0;i<n;i++)
+{
 turn[i]+=wait[i];
+}
 
 for(i=0;i<n;i++)
 {
@@ -80,7 +87,7 @@ tt=(float)(st/n);
 printf("Process number		Burst time		Wait time		Turaround time\n");
 for(i=0;i<n;i++)
 {
-printf("%d		 	%d 			%d 			%d\n",pnum[i],burst[i],wait[i],turn[i]);
+printf("%d		 	%d 			%d 			%d\n",pnum[i],b[i],wait[i],turn[i]);
 }
 printf("Turnaround time : %f\n",tput);
 printf("Average waiting time: %f\n",wt);
